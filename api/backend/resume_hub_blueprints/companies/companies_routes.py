8103d36d316eeb55ffb37a6c043d2ab51f8ccdb1
Aidan Gilchrist <gilchrist.ai@northeastern.edu>
2024-11-30 21:13:25 -0500

    companies get w/ id tested

--- api/backend/resume_hub_blueprints/companies/companies_routes.py
+++ api/backend/resume_hub_blueprints/companies/companies_routes.py
@@ -68,8 +68,20 @@ def post_company():
 # GET /companies/{company_id}
 @companies.route('/companies/<int:company_id>', methods=['GET'])
 def get_company(company_id):
-    # TODO: Implement
-    return None
+    query = '''
+    SELECT *
+    FROM Company
+    WHERE Id= {0}'''.format(company_id)
+    
+
+    cursor=db.get_db().cursor()
+    cursor.execute(query)
+    theData=cursor.fetchall()
+
+    response = make_response(jsonify(theData))
+    response.status_case=200
+    return response
+
 
 
 # PUT /companies/{company_id}
