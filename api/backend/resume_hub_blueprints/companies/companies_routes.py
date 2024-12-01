fabd67eae3243054345eb432c33e733a20035a70
Aidan Gilchrist <gilchrist.ai@northeastern.edu>
2024-11-30 20:04:19 -0500

    companies get 1

--- api/backend/resume_hub_blueprints/companies/companies_routes.py
+++ api/backend/resume_hub_blueprints/companies/companies_routes.py
@@ -1,13 +1,28 @@
 from flask import Blueprint
 
 companies = Blueprint('companies', __name__)
-
+from flask import request
+from flask import jsonify
+from flask import make_response
+from flask import current_app
+from backend.db_connection import db
+from backend.ml_models.model01 import predict
 
 # GET /companies
 @companies.route('/companies', methods=['GET'])
 def get_companies():
-    # TODO: Implement
-    return None
+    query = '''
+    SELECT *
+    FROM Company
+    '''
+
+    cursor=db.get_db().cursor()
+    cursor.execute(query)
+    theData=cursor.fetchall()
+
+    response = make_response(jsonify(theData))
+    response.status_case=200
+    return response
 
 
 # POST /companies
