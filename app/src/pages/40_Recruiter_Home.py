6cb1f09393585462173ab7b094621fd2af87dda7
DanTheCoder <danielkaplan812@gmail.com>
2024-12-04 12:43:43 -0500

    Removed Specific Resume

--- app/src/pages/40_Recruiter_Home.py
+++ app/src/pages/40_Recruiter_Home.py
@@ -27,11 +27,6 @@ def getId():
 
 st.session_state['recruiter_id'] = getId()
 
-if st.button('View Specific Resumes',
-             type='primary',
-             use_container_width=True):
-    st.switch_page('pages/41_Resume_List.py')
-
 if st.button('View Resumes by Skills',
              type='primary',
              use_container_width=True):
